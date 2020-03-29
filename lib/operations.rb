8113138c8d4b6d35703c41db39634585a2f1a52c
John Carter <johncarter352@gmail.com>
2020-03-29 02:30:24 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,17 +1,17 @@
 def unsafe?(speed)
   if speed > 60
-    "true"
+    true
     elsif speed < 40
-    "true"
+    true
   else
-    "false"
+    false
   end
 end
 
 
 
 def not_safe?(speed)
-	speed 40>60 ? "true" : "false"
+	speed 40>60 ? true : false
 end
 
 	
